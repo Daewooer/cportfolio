167319ba78f39613d973f53f141665e026ffb2ca
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-06-24 17:28:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+int main(void) {
+    int MyAge = 12;
+    int MyBirthday = 26/11/2009;
+    char MyName = "Kostas Zafolias";
+    //main section
+    printf("Hello I am Kostas and this is my portfolio");
+    printf("My name is = %c\n", MyName);
+    printf("My age is = %d\n", MyAge);
+    printf("My BDay is = %d\n", MyBirthday);
+}
